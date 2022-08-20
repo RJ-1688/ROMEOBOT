6e3caa213f8378d9005f151c3a065109ff7c241c
Oyehoye12 <111141033+Oyehoye12@users.noreply.github.com>
2022-08-20 11:10:56 +0530

    Update hell_string.py

--- hell_string.py
+++ hell_string.py
@@ -6,26 +6,26 @@ from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
 
-print("•••   UltronBot  SESSION  GENERATOR   •••")
-print("\nHello!! Welcome to UltronBot Session Generator\n")
+print("•••   ROMEO SESSION  GENERATOR   •••")
+print("\nHello!! Welcome to RomeoBot Session Generator\n")
 okvai = input("Enter 69 to continue: ")
 if okvai == "69":
-    print("Choose the string session type: \n1. UltronBot \n2. Music Bot")
+    print("Choose the string session type: \n1. RomeoBot \n2. Music Bot")
     library = input("\nYour Choice: ")
     if library == "1":
-        print("\nTelethon Session For UltronBot")
+        print("\nTelethon Session For RomeoBot")
         APP_ID = int(input("\nEnter APP ID here: "))
         API_HASH = input("\nEnter API HASH here: ")
-        with TelegramClient(StringSession(), APP_ID, API_HASH) as UltronBot:
-            print("\nYour UltronBot Session Is sent in your Telegram Saved Messages.")
-            UltronBot.send_message("me", f"#UltronBot #UltronBot_SESSION \n\n`{UltronBot.session.save()}`")
+        with TelegramClient(StringSession(), APP_ID, API_HASH) as RomeoBot:
+            print("\nYour RomeoBot Session Is sent in your Telegram Saved Messages.")
+            RomeoBot.send_message("me", f"#RomeoBot #RomeoBot_SESSION \n\n`{RomeoBot.session.save()}`")
     elif library == "2":
         print("Pyrogram Session for Music Bot")
         APP_ID = int(input("\nEnter APP ID here: "))
         API_HASH = input("\nEnter API HASH here: ")
         with Client(':memory:', api_id=APP_ID, api_hash=API_HASH) as UltronBot:
-            print("\nYour UltronBot Session Is sent in your Telegram Saved Messages.")
-            UltronBot.send_message("me", f"#UltronBot_MUSIC #UltronBot_SESSION\n\n`{UltronBot.export_session_string()}`")
+            print("\nYour RomeoBot Session Is sent in your Telegram Saved Messages.")
+            RomeoBot.send_message("me", f"#RomeoBot_MUSIC #RomeoBot_SESSION\n\n`{RomeoBot.export_session_string()}`")
     else:
         print("Please Enter 1 or 2 only.")
 else:
