904b86c0078701d727128bbd4c9e852e5f21466a
Oyehoye12 <111141033+Oyehoye12@users.noreply.github.com>
2022-08-20 12:16:33 +0530

    Update decs.py

--- RomeoBot/clients/decs.py
+++ RomeoBot/clients/decs.py
@@ -5,9 +5,9 @@ from pathlib import Path
 from telethon import events
 
 from .session import H2, H3, H4, H5
-from UltronBot import CMD_LIST, LOAD_PLUG, bot
-from UltronBot.config import Config
-from UltronBot.sql.gvar_sql import gvarstat
+from RomeoBot import CMD_LIST, LOAD_PLUG, bot
+from RomeoBot.config import Config
+from RomeoBot.sql.gvar_sql import gvarstat
 
 
 def hell_cmd(
