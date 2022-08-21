57c7c923791d2739068fa181d3f920e31f508cb5
Oyehoye12 <111141033+Oyehoye12@users.noreply.github.com>
2022-08-21 09:38:30 +0530

    Update clone.py

--- RomeoBot/plugins/clone.py
+++ RomeoBot/plugins/clone.py
@@ -5,7 +5,7 @@ from telethon.tl import functions
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 
-from UltronBot.sql.gvar_sql import addgvar, gvarstat, delgvar
+from RomeoBot.sql.gvar_sql import addgvar, gvarstat, delgvar
 from . import *
 
 
