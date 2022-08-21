c89b513c1c5789299980d98874b19482094a2ee7
Oyehoye12 <111141033+Oyehoye12@users.noreply.github.com>
2022-08-21 18:12:13 +0530

    Update uff.py

--- RomeoBot/plugins/uff.py
+++ RomeoBot/plugins/uff.py
@@ -2,14 +2,21 @@ from . import *
 
 
 @hell_cmd(pattern="Uff")
-async def _(_, message: Message):
-    targetcontent = message.reply_to_message
-    downloadtargetcontent = await client.download_media(targetcontent)
-    send = await client.send_document("me", downloadtargetcontent)
-    os.remove(downloadtargetcontent)
-
-
-
+async def _(event):
+    if not event.is_reply:
+        return await event.edit("Reply to a self distructing pic !.!.!")
+    k = await event.get_reply_message()
+    pic = await k.download_media()
+    await bot.send_file(
+        event.chat_id,
+        pic,
+        caption=f"""
+  OwO!! LoL, Destruction Mode Pic Destroyed!!
+  Pic captured By Lêɠêɳ̃dẞø†
+🌚🌝
+  """,
+    )
+    await event.delete()
 CmdHelp("Uff").add_command(
     "Uff", "This Command Can save documents"
 ).add_info("uff").add_warning("✅ Harmless Module.").add()
