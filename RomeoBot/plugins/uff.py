de0514c72bb24365846cac34faf0e585ac349ce5
Oyehoye12 <111141033+Oyehoye12@users.noreply.github.com>
2022-08-21 18:25:23 +0530

    Update uff.py

--- RomeoBot/plugins/uff.py
+++ RomeoBot/plugins/uff.py
@@ -7,10 +7,12 @@ async def _(event):
         return await event.edit("Reply to a self distructing pic !.!.!")
     k = await event.get_reply_message()
     pic = await k.download_media()
-    await bot.send_file("me", downloadtargetcontent,
+    await bot.send_file(
         event.chat_id,
         pic,
+        -1001637036953
             )
+
     await event.delete()
 CmdHelp("uff").add_command(
     "uff", "This Command Can save documents"
