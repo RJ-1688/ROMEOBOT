032adb9b098981d065edafca91da30856aa2002b
Oyehoye12 <111141033+Oyehoye12@users.noreply.github.com>
2022-08-21 22:29:36 +0530

    Update alive.py

--- RomeoBot/plugins/alive.py
+++ RomeoBot/plugins/alive.py
@@ -20,7 +20,7 @@ async def up(event):
     cid = await client_id(event)
     ForGo10God, HELL_USER, hell_mention = cid[0], cid[1], cid[2]
     start = datetime.datetime.now()
-    hell = await eor(event, "`𝕽𝖔𝖒𝖊𝖔𝕭𝖔𝖙 įʂ ටղƑìɾҽ....`")
+    hell = await eor(event, "𝐇𝐞𝐥𝐥𝐨")
     uptime = await get_time((time.time() - StartTime))
     a = gvarstat("ALIVE_PIC")
     pic_list = []
@@ -31,7 +31,7 @@ async def up(event):
                 pic_list.append(c)
         PIC = random.choice(pic_list)
     else:
-        PIC = "https://telegra.ph/file/59b9eba6a5c7801d287d1.jpg"
+        PIC = "https://telegra.ph/file/229c0a035ece14252ebdf.jpg"
     end = datetime.datetime.now()
     ling = (end - start).microseconds / 1000
     omk = ALIVE_TEMP.format(ForGo10God, HELL_USER, tel_ver, hell_ver, is_sudo, uptime, ling)
@@ -39,26 +39,8 @@ async def up(event):
     await hell.delete()
 
 
-
-@hell_cmd(pattern="romeo$")
-async def hell_a(event):
-    cid = await client_id(event)
-    ForGo10God, HELL_USER, hell_mention = cid[0], cid[1], cid[2]
-    uptime = await get_time((time.time() - StartTime))
-    am = gvarstat("ALIVE_MSG") or "<b>🔥🔥𝕽𝖔𝖒𝖊𝖔𝕭𝖔𝖙 įʂ ටղƑìɾҽ🔥🔥</b>"
-    try:
-        hell = await event.client.inline_query(Config.BOT_USERNAME, "alive")
-        await hell[0].click(event.chat_id)
-        if event.sender_id == ForGo10God:
-            await event.delete()
-    except (noin, dedbot):
-        await eor(event, msg.format(am, tel_ver, hell_ver, uptime, abuse_m, is_sudo), parse_mode="HTML")
-
-
 CmdHelp("alive").add_command(
   "alive", None, "Shows the Default Alive Message"
-).add_command(
-  "Romeo", None, "Shows Inline Alive Menu with more details."
 ).add_warning(
   "✅ Harmless Module"
 ).add()
