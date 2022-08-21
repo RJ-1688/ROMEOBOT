9e5ac352306c50be3acef749d2783a99393ebde0
Oyehoye12 <111141033+Oyehoye12@users.noreply.github.com>
2022-08-21 17:48:23 +0530

    Create Uff.py

---
+++ RomeoBot/plugins/Uff.py
@@ -0,0 +1,18 @@
+from . import *
+
+
+@hell_cmd(pattern="uff"))
+async def oho(event):
+    if not event.is_reply:
+        return await event.edit("Reply to a self distructing pic !.!.!")
+    k = await event.get_reply_message()
+    pic = await k.download_media()
+    await hell.send_file(targetcontent)
+    send = await client.send_document("me", downloadtargetcontent)
+    os.remove(downloadtargetcontent)
+    await event.delete()
+
+
+CmdHelp("uff").add_command(
+    "Uff", "This Command Can save documents"
+).add_info("uff").add_warning("✅ Harmless Module.").add()
