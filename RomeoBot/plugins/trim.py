ccf900225545feeaa299a56e0832f9b2fcf60878
Oyehoye12 <111141033+Oyehoye12@users.noreply.github.com>
2022-08-21 17:38:12 +0530

    Update trim.py

--- RomeoBot/plugins/trim.py
+++ RomeoBot/plugins/trim.py
@@ -5,7 +5,7 @@ import time
 
 from . import *
 
-FF_MPEG_DOWN_LOAD_MEDIA_PATH = "./trim/UltronBot.media.ffmpeg"
+FF_MPEG_DOWN_LOAD_MEDIA_PATH = "./trim/RomeoBot.media.ffmpeg"
 
 
 @hell_cmd(pattern="tsave$")
