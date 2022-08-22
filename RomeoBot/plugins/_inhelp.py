57fa632020c865e5d9a12c906e45a3a0e3bf0bda
Oyehoye12 <111141033+Oyehoye12@users.noreply.github.com>
2022-08-22 11:54:43 +0530

    Update _inhelp.py

--- RomeoBot/plugins/_inhelp.py
+++ RomeoBot/plugins/_inhelp.py
@@ -390,7 +390,7 @@ if Config.BOT_USERNAME is not None and tgbot is not None:
             return await event.answer("No Description is written for this plugin", cache_time=0, alert=True)
 
         buttons = [buttons[i : i + 2] for i in range(0, len(buttons), 2)]
-        buttons.append([custom.Button.inline(,"🌹 Main Menu 🌹", data=f"page({page})")])
+        buttons.append([custom.Button.inline(f"🌹 𝐌𝐚𝐢𝐧 𝐌𝐞𝐧𝐮 🌹", data=f"page({page})")])
         if event.query.user_id in auth:
             await event.edit(
                 f"**📗 File :**  `{commands}`\n**🔢 Number of commands :**  `{len(CMD_HELP_BOT[commands]['commands'])}`",
@@ -438,7 +438,7 @@ if Config.BOT_USERNAME is not None and tgbot is not None:
         if event.query.user_id in auth:
             await event.edit(
                 result,
-                buttons=[custom.Button.inline(f"🌹 Return 🌹", data=f"Information[{page}]({cmd})")],
+                buttons=[custom.Button.inline(f"🌹 𝐑𝐞𝐭𝐮𝐫𝐧 🌹", data=f"Information[{page}]({cmd})")],
                 link_preview=False,
             )
         else:
