d92040e492522f00394a07f7591e04a363ee8910
RJ-1688 <114766921+RJ-1688@users.noreply.github.com>
2022-10-06 23:51:11 +0530

    Update and rename ex_config.py to config.py

--- config.py
+++ config.py
@@ -4,24 +4,24 @@ from RomeoBot.config.hell_config import Config
 
 class Development(Config):
   # get these values from my.telegram.org. 
-  APP_ID = 9    # 9 is a placeholder. Fill your 6 digit api id
-  API_HASH = "53e10a257baaddea9c413a0c4249e731"   # replace this with your api hash
+  APP_ID = 18551764    # 9 is a placeholder. Fill your 6 digit api id
+  API_HASH = "991a66d509729ab73929bb42b514dce5"   # replace this with your api hash
 
   # create any PostgreSQL database.
   # I recommend to use elephantsql and paste that link here
-  DB_URI = ""
+  DB_URI = "postgres://wdlsnpmj:Me698p_QlBJsZdpcaXCVt3hjVICZDkrC@mouse.db.elephantsql.com/wdlsnpmj"
   ALIVE_NAME = "Romeo"
   # After cloning the repo and installing requirements...
   # Do `python string.py` and fill the on screen prompts.
   # String session will be saved in your saved message of telegram.
   # Put that string here.
-  ROMEOBOT_SESSION = "Your value"
+  ROMEOBOT_SESSION = "1BVtsOJIBu6l-XVj70_qR-j1FjzNVWyZr3r-CSE-Eo-nBJmTCifjYCZJh6vrrWK2Qyr0ILnSW_A6KSy5BMKL82aI-5MiNFVrZ0bn_4aoOIVPghIBLMMsqBNWVpfEpCsV25tekhpTa5oMmWiYd7ATq-jHqRzvH89XALr7Y7RGO3xIINYctMhnYA3oPavStkihMxEp7P3fZ7649GjUbAS_SiyLGryovxQMoNinChRn1ZcVi4MnYdxXOI2yvNScgufg9G2qRg_XU3FjxcIh4t_4yI0MyLtim4knoGirDL47F1ZFtYTyX2qLV6UeWH7bpva1vF7pEjOSGMo2CLxo8YoYYE4xmz-5HVKE="
 
   # Create a bot in @BotFather
   # And fill the following values with bot token and username.
-  BOT_TOKEN = "Your value" #token
+  BOT_TOKEN = "5604091752:AAEbdcctzFVW_eET0jWCbA8IYl7OEoOnHos" #token
   #FILL BOT USERNAME WITHOUT @
-  BOT_USERNAME = "Your value"
+  BOT_USERNAME = "Uff_ye_Adaaaabot"
   # Custom Command Handler. 
   HANDLER = "."
  
